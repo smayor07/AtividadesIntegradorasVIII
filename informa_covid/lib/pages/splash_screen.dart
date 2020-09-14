ee879f42385b0050996d30428de19881d5bffd5c
RodolfoPaiva <rodolfopaiva01@outlook.com>
2020-09-14 20:27:26 -0300

    Rodolfo_commit1_TelaPreta

--- informa_covid/lib/pages/splash_screen.dart
+++ informa_covid/lib/pages/splash_screen.dart
@@ -12,7 +12,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(
-        Duration(seconds: 5),
+        Duration(seconds: 2),
         () => Navigator.push(
             context, MaterialPageRoute(builder: (context) => HomePage())));
   }
